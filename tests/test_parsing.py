2f5ec3637dedf1fd075e73c98aec45002353b6a1
raezero <raezero@kisti.re.kr>
2026-03-04 22:52:18 +0900

    Delete tests directory

--- tests/test_parsing.py
+++
@@ -1,4 +0,0 @@
-def test_scienceon_parse_invalid_xml_returns_error(kisti_server_module):
-    client = kisti_server_module.ScienceONClient()
-    result = client._parse_xml_response("not xml")
-    assert result.get("error") is True
